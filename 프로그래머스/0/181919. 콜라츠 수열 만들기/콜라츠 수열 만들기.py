ac504897d1234da1be0801a8dd5170551ab86139
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-08-19 01:36:26 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = [n]
+    x = n
+    
+    while x != 1:
+        if x % 2 == 0:
+            x = x // 2
+        else:
+            x = 3 * x + 1
+            
+        answer.append(x)
+    
+    return answer
