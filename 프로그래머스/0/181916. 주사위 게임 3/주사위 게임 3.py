563bba944a2e7b3ee3588e3d3f8cb44444dbd301
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-08-20 10:42:48 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -4,23 +4,23 @@ def solution(a, b, c, d):
     # 리스트에서 서로 다른 숫자들 (중복 제거)
     unique = sorted(set(l))
     
-    if len(unique) == 1:
-        # 다 같음: [4]
+    if len(unique) == 1: # 다 같음: [4]
         p = unique[0]
         return 1111 * p
     
-    elif len(unique) == 4:
-        # 다 다름: [1,1,1,1]
+    elif len(unique) == 4: # 다 다름: [1,1,1,1]
         return l[0]
     
     elif len(unique) == 2:
         p, q = unique
+
+        # 세 개 같고 하나 다름: [1,3]
         if l.count(p) == 3 or l.count(q) == 3:
-            # 세 개 같고 하나 다름: [1,3]
             p, q = (p, q) if l.count(p) == 3 else (q, p)
+
             return (10 * p + q) ** 2
+        # 두 쌍: [2,2]
         else:
-            # 두 쌍: [2,2]
             return (p + q) * abs(p - q)
     
     else:  # len(unique) == 3
