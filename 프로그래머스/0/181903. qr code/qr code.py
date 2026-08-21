c82ad0a8f7f36e89687b228642fad74695b8044c
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-08-21 15:53:14 +0900

    [level 0] Title: qr code, Time: 0.08 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.py
+++ 프로그래머스/0/181903. qr code/qr code.py
@@ -1,7 +1,9 @@
 def solution(q, r, code):
-    result = ''
-    for idx, val in enumerate(code):
-        if idx % q == r:
-            result += val
-            
+    # result = ''
+    # for idx, val in enumerate(code):
+    #     if idx % q == r:
+    #         result += val
+
+    result = ''.join(val for idx, val in enumerate(code) if idx % q == r)
+    
     return result
