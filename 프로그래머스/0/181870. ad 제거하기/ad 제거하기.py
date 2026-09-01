6541322c14d43f052590ebd26e5ccdb99cdc02af
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-09-02 00:01:21 +0900

    [level 0] Title: ad 제거하기, Time: 0.05 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,9 @@
+def solution(strArr):
+#     result = []
+#     for i in strArr:
+#         if i.find('ad') == -1:
+#             result.append(i)
+            
+#     return result
+    
+    return [i for i in strArr if i.find('ad') == -1]
