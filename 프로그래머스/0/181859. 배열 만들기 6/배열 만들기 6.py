7eedf709d04c612179d7a29ec43ff34575f07758
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-09-03 09:49:17 +0900

    [level 0] Title: 배열 만들기 6, Time: 220.21 ms, Memory: 19.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,18 @@
+def solution(arr):
+    stk = []
+    i = 0
+    
+    if i < len(arr):
+        for num in arr:
+            if len(stk) == 0:
+                stk.append(arr[i])
+                i += 1
+            else:
+                if stk[-1] == arr[i]:
+                    stk = stk[:-1]
+                    i += 1
+                else:
+                    stk.append(arr[i])
+                    i += 1
+                    
+    return stk if len(stk) != 0 else [-1]
