57a5be033ffb75c5f448a6d0767539892d6dfbf0
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-09-04 14:15:23 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,9 +1,12 @@
 def solution(n):
-    result = []
-    for i in range(n):
-        if i % 2 == 0:
-            result.append('수')
-        else:
-            result.append('박')
+#     result = []
+#     for i in range(n):
+#         if i % 2 == 0:
+#             result.append('수')
+#         else:
+#             result.append('박')
             
-    return ''.join(result)
+#     return ''.join(result)
+
+    str = '수박' * n
+    return str[:n]
