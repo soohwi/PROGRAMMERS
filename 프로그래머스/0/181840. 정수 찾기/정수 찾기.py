5b1d4580fe540630e75ca2419083489c50c77eac
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-09-10 00:40:22 +0900

    [level 0] Title: 정수 찾기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.py
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.py
@@ -1,7 +1,9 @@
 def solution(num_list, n):
-    result = 0
-    for i in num_list:
-        if i == n:
-            result = 1
+#     result = 0
+#     for i in num_list:
+#         if i == n:
+#             result = 1
             
-    return result
+#     return result
+
+    return int(n in num_list)
