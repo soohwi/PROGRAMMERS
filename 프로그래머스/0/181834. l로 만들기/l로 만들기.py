59fe51bccddf863a169f4dc92ba37573ad15f84c
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-09-11 00:13:07 +0900

    [level 0] Title: l로 만들기, Time: 4.30 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,4 +1,3 @@
 def solution(myString):
-    alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
 
-    return ''.join(['l' if i in alp else i for i in list(myString)])
+    return ''.join(['l' if i < 'l' else i for i in myString])
