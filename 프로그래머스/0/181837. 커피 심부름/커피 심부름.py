bb5cdbfb0ae423d2f4af395177f43c2466dbd09e
Soohwi <40660792+soohwi@users.noreply.github.com>
2026-09-10 23:23:14 +0900

    [level 0] Title: 커피 심부름, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,11 +1,9 @@
 def solution(order):
     result = 0
     for i in order:
-        if 'americano' in i:
-            result += 4500
-        elif 'latte' in i:
-            result += 5000
-        else:
-            result += 4500
+        if 'latte' in i:
+            result += 500
+        
+        result += 4500
         
     return result
